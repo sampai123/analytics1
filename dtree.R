9f471c1a3118d4ab1892f4d33d1ce8bf0220bd17
unknown <tpaisamartha@gmail.com>
2018-10-27 15:22:57 +0530

    25/10

---
+++ dtree.R
@@ -0,0 +1,13 @@
+library (rpart)
+library (rpart.plot)
+gender=sample(c('M','F'),size=10000, prob=c(.6,.4),replace=T)
+age= ceiling(rnorm(1000,35,5))
+buy= sample (c('Yes','No'),size=10000,prob=c(.5,.5),replace=T)
+df= data.frame(buy,age,gender)
+head(df)
+
+dtree1 = rpart(buy~gender+age, data=df, minsplit=10, minbucket=4, cp=.005)
+dtree1
+dtree1$variable.importance
+rpart.plot(dtree1, cex=.7)
+
